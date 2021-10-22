ec3ccd743cabe4d06fb10c9a081773aecafcd9b6
martacardini <92572635+martacardini@users.noreply.github.com>
2021-10-22 10:55:10 +0200

    Update R_first_code.r

--- R_first_code.r
+++ R_first_code.r
@@ -23,3 +23,35 @@ streams
 
 # to visualise the table we use the fonction View 
 View(streams)
+
+# From now on, we are going to import and export data
+# setting working directory (setwd fonction)
+
+setwd("C:/lab/")
+
+data.frame(water, fishes)
+
+streams <-data.frame(water, fishes)
+
+# let's export our table
+# https://www.rdocumentation.org/packages/utils/versions/3.6.2/topics/write.table
+write.table(streams, file = "my_first_table.txt")
+
+# how to import a table into R
+read.table("my_first_table.txt")
+# let's assign it to an object into R
+ducciotable <- read.table("my_first_table.txt")
+
+# the first statistics 
+# do a summary of the dataset
+# https://www.rdocumentation.org/packages/base/versions/3.6.2/topics/summary
+summary(ducciotable)
+
+#marta does not like water
+# marta wants info only on fishes
+summary(ducciotable$fishes)
+
+# let's make an histogram
+hist(ducciotable$fishes)
+hist(ducciotable$water)
+
