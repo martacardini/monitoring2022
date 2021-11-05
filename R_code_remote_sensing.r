8d72972d9067410e943331e675608abe9083e672
martacardini <92572635+martacardini@users.noreply.github.com>
2021-11-05 10:10:08 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -9,6 +9,7 @@
 
 library(raster)
 
+# use the brick function
 # https://www.rdocumentation.org/packages/raster/versions/3.4-13/topics/brick
 
 setwd("C:/lab")
@@ -28,12 +29,30 @@ plot(l2011)
 # B1 is the reflectance in the blue band
 # B2 is the reflectance in the green band
 # B3 is the reflectance in the red band
+# B4 is the reflectance in the near infrared (NIR) band
 
 # colorRampPalette function: chances color we see
+# https://www.rdocumentation.org/packages/dichromat/versions/1.1/topics/colorRampPalette
+
 # 100 means the number pf tones we have in our palette
 cl <- colorRampPalette(c("black", "grey", "light grey"))(100)
 plot(l2011, col=cl)
 
 # plotting the set of bands to see the image in colours
+# https://www.rdocumentation.org/packages/raster/versions/3.5-2/topics/plotRGB
+
 plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+
+#day2 (05/11/2021)
+
+# we're gonna use the B4- NIR
+# plot the green band B2
+# the green band is called B2_sre, it is inside l2011
+
+plot(l2011$B2_sre)
+
+
+
+
+
 
