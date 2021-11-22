d531147bda513c7fc903d444b3d8941ace6ff6fb
martacardini <92572635+martacardini@users.noreply.github.com>
2021-11-22 10:59:06 +0100

    Update R_code_energy_rs.r

--- R_code_energy_rs.r
+++ R_code_energy_rs.r
@@ -14,8 +14,8 @@ l1992
 
 # Bands: defor1_.1, defor1_.2, defor1_.3 
 # plotRGB
-plotRGB(l1992, r="defor1_.1", g="defor1_.2", b="defor1_.3", stretch="Lin")
-# plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
+#plotRGB(l1992, r="defor1_.1", g="defor1_.2", b="defor1_.3", stretch="Lin")
+plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
 
 
 # defor1_.1 = NIR
@@ -25,3 +25,64 @@ plotRGB(l1992, r="defor1_.1", g="defor1_.2", b="defor1_.3", stretch="Lin")
 # change colors
 plotRGB(l1992, r=3, g=2, b=1, stretch="Lin")
 plotRGB(l1992, r="defor1_.2", g="defor1_.1", b="defor1_.3", stretch="Lin")
+
+# importing the data 
+l2006 <- brick ("defor2_.jpg") # image from 2006
+
+# plot RGB
+plotRGB(l2006, r=1, g=2, b=3, stretch="Lin")
+
+#plotting the images from 1992 and 2006 together
+# par()
+par(mfrow = c(2,1))   # 2 rows 1 column
+plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
+plotRGB(l2006, r=1, g=2, b=3, stretch="Lin")
+
+# calculate the energy in 1992
+dev.off
+dvi1992 <- l1992$defor1_.1 - l1992$defor1_.2
+
+# color Ramp Palette
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
+
+plot(dvi1992, col= cl)
+
+# claculate energy in 2006
+dvi2006 <- l2006$defor2_.1 - l2006$defor2_.2
+plot(dvi2006, col = cl)
+
+#plot both together
+par(mfrow = c(2,1)) 
+plot(dvi1992, col= cl)
+plot(dvi2006, col = cl)
+
+# the energy lost is shown in yellow because it's a color that catches our eyes
+
+# calculate the loss of energy in the time scale
+# differencing two images of energy in two different times
+
+dvidif <- dvi1992 - dvi2006
+# plot the result
+cld <- colorRampPalette(c('blue','white','red'))(100)
+plot(dvidif, col= cld)
+
+# red= high level of difference in which energy has been lost, passed from a high value in 1992 to a low value in 2006
+
+
+#final plot: original images, dvis, final dvi differences.
+par(mfrow = c(3,2))
+plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
+plotRGB(l2006, r=1, g=2, b=3, stretch="Lin")
+plot(dvi1992, col= cl)
+plot(dvi2006, col = cl)
+plot(dvidif, col= cld)
+
+#export the final result
+pdf("energy.pdf", width = 13, height = 9)
+par(mfrow = c(3,2))
+plotRGB(l1992, r=1, g=2, b=3, stretch="Lin")
+plotRGB(l2006, r=1, g=2, b=3, stretch="Lin")
+plot(dvi1992, col= cl)
+plot(dvi2006, col = cl)
+plot(dvidif, col= cld)
+dev.off()
